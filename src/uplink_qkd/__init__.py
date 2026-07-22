3696e6df0fa809754a652b1a1be56fa5530c39b1
Faris Redza <faris.redza1999@gmail.com>
2026-07-22 10:32:40 +0100

    included loss parsing in init

--- src/uplink_qkd/__init__.py
+++ src/uplink_qkd/__init__.py
@@ -2,6 +2,7 @@
 
 from .finite_key import brute_search_parallel_equality, objective, smart_optimise
 from .rates import raw_overpass, raw_overpass_cutoff, raw_overpass_instant, secure_key_rates
+from .loss_parse import LossProfile, get_loss_profile, get_loss_profiles, optimal_power_curve
 
 __all__ = [
     "brute_search_parallel_equality",
@@ -11,4 +12,8 @@ __all__ = [
     "raw_overpass_instant",
     "secure_key_rates",
     "smart_optimise",
+    "LossProfile",
+    "get_loss_profile",
+    "get_loss_profiles",
+    "optimal_power_curve"
 ]
